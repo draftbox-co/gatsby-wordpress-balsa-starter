6f64c15e3c5433f86cac511af67f273943f3701f
arunpriyadarshi <arunpriyadarshi52@gmail.com>
2020-07-21 13:52:59 +0530

    changed integration plugins and changed underlying packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ if (process.env.GA) {
 }
 
 if (process.env.GATSBY_MIXPANEL_TOKEN) {
-  gatsbyPlugins.unshift({
+  gatsbyPlugins.push({
     resolve: `gatsby-plugin-mixpanel`,
     options: {
       apiToken: process.env.GATSBY_MIXPANEL_TOKEN,
@@ -78,11 +78,12 @@ if (process.env.GATSBY_MIXPANEL_TOKEN) {
 }
 
 if (process.env.GATSBY_HOTJAR_ID) {
-  gatsbyPlugins.unshift({
-    resolve: `gatsby-plugin-hotjar`,
+  gatsbyPlugins.push({
+    resolve: `@draftbox-co/gatsby-plugin-hotjar-lazy`,
     options: {
       id: process.env.GATSBY_HOTJAR_ID,
-      sv: 6
+      sv: 6,
+      optimize: true,
     }
   });
 }
@@ -98,29 +99,32 @@ if (process.env.GATSBY_GTAG_MANAGER_ID) {
 }
 
 if (process.env.GATSBY_TAWK_ID) {
-  gatsbyPlugins.unshift({
-    resolve: `gatsby-plugin-tawk`,
+  gatsbyPlugins.push({
+    resolve: `@draftbox-co/gatsby-plugin-tawk-lazy`,
     options: {
       tawkId: process.env.GATSBY_TAWK_ID,
+      optimize: true,
     }
   });
 }
 
 if (process.env.GATSBY_CRISP_ID) {
-  gatsbyPlugins.unshift({
-    resolve: `gatsby-plugin-crisp-chat`,
+  gatsbyPlugins.push({
+    resolve: `@draftbox-co/gatsby-plugin-crisp-chat-lazy`,
     options: {
       websiteId: process.env.GATSBY_CRISP_ID,
-      enableDuringDevelop: true
+      enableDuringDevelop: true,
+      optimize: true,
     }
   });
 }
 
 if (process.env.GATSBY_OLARK_ID) {
-  gatsbyPlugins.unshift({
-    resolve: `gatsby-plugin-olark`,
+  gatsbyPlugins.push({
+    resolve: `@draftbox-co/gatsby-plugin-olark-lazy`,
     options: {
       olarkSiteID: process.env.GATSBY_OLARK_ID,
+      optimize: true,
     }
   });
 }
